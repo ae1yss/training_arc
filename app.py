b090cc823b70b310f697f152af5d8257939aef00
ae1yss <ae1yss@outlook.com>
2022-09-11 21:04:06 -0500

    first endpoint + style

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
-tako = Flask(__name__)
- 
-@tako.route('/')
+from flask import Flask, render_template
+import calendar
 
+tako = Flask(__name__)
+nombre = "Juan"
+
+@tako.route('/')
 def helloworld():
-    return "hello world"
+    return render_template("a.html", nombre = nombre)
+ 
+@tako.route('/tako.html')
+def helloworld2():
+    return render_template("tako.html")
+
 
 #port = 8080
 
